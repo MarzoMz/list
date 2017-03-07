c70e80a7993caf40815478ba305c1782dbd5dbf7
Dzmitry Alkhimovich <dm.alkhimovich@gmail.com>
2017-03-07 14:16:01 +0300

    Добавлена вложенность списков

--- list.js
+++ list.js
@@ -20,17 +20,18 @@ var drinks = [
 
 function createList(data) {
   var ul = document.createElement('ul');
-  document.body.appendChild(ul);
   data.forEach(function(item) {
     var li = document.createElement('li');
     ul.appendChild(li);
     var liData = document.createTextNode(item['caption']);
     li.appendChild(liData);
     if (item.children) {
-      createList(item.children);
+      var childList = createList(item.children);
+      ul.appendChild(childList);
     }
   });
+  return ul;
 }
 
 var list = createList(drinks);
-//document.body.appendChild(list);
+document.body.appendChild(list);
